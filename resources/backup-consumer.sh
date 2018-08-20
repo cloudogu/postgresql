f7a245ad62f20f41ec52531b45c5b857778d015c
Christoph Wolfes <christoph.wolfes@triology.de>
2018-08-20 15:24:47 +0200

    updates error handling
    Exit script with error when not all parameters are provided

--- resources/backup-consumer.sh
+++ resources/backup-consumer.sh
@@ -10,11 +10,12 @@ if [[ ! -z $1 && ! -z $2 ]]; then
   # service account 
   username="$1";
   database_name="$2";
-  
+
   # database dump printed on stdOut
   pg_dump -U $username -d $database_name
 
 else 
-  echo "ERROR: please provide following parameters: (1) username, (2) database_name"
+  echo "Please provide following parameters: (1) username, (2) database_name" >&2;
+  exit 1;
 fi
 

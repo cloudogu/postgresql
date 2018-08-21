abc19e6be0557ba2364fe29cf79a64124a24ff55
Christoph Wolfes <christoph.wolfes@triology.de>
2018-08-21 13:17:08 +0200

    changes script input from parameters to stdIn
    Required input sample:
    echo "username: XYZ"
    echo "database: ABC"

--- resources/backup-consumer.sh
+++ resources/backup-consumer.sh
@@ -5,17 +5,26 @@
 set -o errexit
 set -o pipefail
 
-# continue when all parameters are provided
-if [[ ! -z $1 && ! -z $2 ]]; then
-  # service account 
-  username="$1";
-  database_name="$2";
+# read service account keys from stdin
+while read -r line; do
+  case $line in
+  username:*)
+  username=$(echo $line | sed -e "s/username: //g");;
+  database:*)
+  database_name=$(echo $line | sed -e "s/database: //g");;
+  esac
+done
+
+# continue when all service-account keys are provided
+if [[ ! -z $username && ! -z $database_name ]]; then
 
   # database dump printed on stdOut
+  echo "backup data for username: $username and database: $database_name"
+  exit 1;
   pg_dump -U $username -d $database_name
 
-else 
-  echo "Please provide following parameters: (1) username, (2) database_name" >&2;
+else
+  echo "Please provide following service-account keys: username, database" >&2;
   exit 1;
 fi
 

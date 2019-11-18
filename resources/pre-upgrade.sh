32582d835f88e22951c2dde63aeb0384adfe97eb
Robert Auer <robert.auer@cloudogu.com>
2019-11-18 14:14:11 +0100

    Add todo in pre-upgrade script

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -3,4 +3,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# TODO: Only dump database if necessary
+# e.g. at major upgrades, not at bugfix upgrades
 pg_dumpall -U postgres -f "${PGDATA}"/postgresqlFullBackup.dump

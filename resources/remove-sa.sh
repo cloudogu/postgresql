b1d468a0f40128c3645a76db87a92144a56b52b2
Simon Klein <simon.klein@cloudogu.com>
2020-11-20 14:44:24 +0100

    #8 Add command to remove service accounts

---
+++ resources/remove-sa.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SERVICE="${1}"
+if [ X"${SERVICE}" = X"" ]; then
+    echo "usage remove-sa.sh servicename"
+    exit 1
+fi
+
+COLLATION="${2:-}"
+if [[ $COLLATION && ! $COLLATION == *".UTF-8"* ]]; then
+  echo "encodings other than 'UTF-8' are not allowed"
+  exit 1
+fi
+
+# connection user
+ADMIN_USERNAME=$(doguctl config user)
+
+SELECT_DB_NAMES="SELECT datname FROM pg_database WHERE datistemplate=false AND datname like '${SERVICE}\_______'"
+
+for database_name in $(psql -U "${ADMIN_USERNAME}" -t -c "${SELECT_DB_NAMES}")
+do
+  echo "Deleting service account '${database_name}'"
+  psql -U "${ADMIN_USERNAME}" -c "DROP DATABASE if exists ${database_name};" >/dev/null 2>&1
+  psql -U "${ADMIN_USERNAME}" -c "DROP USER if exists ${database_name};" >/dev/null 2>&1
+done

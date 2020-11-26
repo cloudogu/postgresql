b07619576b747a348ce5680be0aa3b85fff177fc
Robert Auer <robert.auer@cloudogu.com>
2020-11-26 14:18:20 +0100

    Bump to postgresql 12.5

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -5,4 +5,6 @@ set -o pipefail
 
 # TODO: Only dump database if necessary
 # e.g. at major upgrades, not at bugfix upgrades
+echo "Dumping database to ${PGDATA}/postgresqlFullBackup.dump..."
 pg_dumpall -U postgres -f "${PGDATA}"/postgresqlFullBackup.dump
+echo "Dump done"

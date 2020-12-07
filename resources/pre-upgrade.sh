de616c5d7d7d9725336d7ec5dd40311129d93f7b
Robert Auer <robert.auer@cloudogu.com>
2020-12-07 08:28:05 +0100

    Showing upgrade notice; dumping database only on major upgrade; #7

--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -3,8 +3,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO: Only dump database if necessary
-# e.g. at major upgrades, not at bugfix upgrades
-echo "Dumping database to ${PGDATA}/postgresqlFullBackup.dump..."
-pg_dumpall -U postgres -f "${PGDATA}"/postgresqlFullBackup.dump
-echo "Dump done"
+FROM_VERSION="${1}"
+TO_VERSION="${2}"
+FROM_MAJOR_VERSION=$(echo "${FROM_VERSION}" | cut -d '.' -f1)
+TO_MAJOR_VERSION=$(echo "${TO_VERSION}" | cut -d '.' -f1)
+
+# dump database if TO_MAJOR_VERSION is equal or higher than 12 and FROM_MAJOR_VERSION is lower than 12
+if [ "12" == "$(printf "%s\\n12" "${TO_MAJOR_VERSION}" | sort -n | head -n1)" ] && [ "12" != "$(printf "%s\\n12" "${FROM_MAJOR_VERSION}" | sort -n | head -n1)" ]; then
+    echo "Dumping database to ${PGDATA}/postgresqlFullBackup.dump..."
+    pg_dumpall -U postgres -f "${PGDATA}"/postgresqlFullBackup.dump
+    echo "Finished dumping database"
+fi
---
+++ resources/upgrade-notification.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+FROM_VERSION="${1}"
+TO_VERSION="${2}"
+
+# print major upgrade notification if TO_VERSION is equal or higher than 12.5-1 and FROM_VERSION is lower than 12.5-1
+if [ "12.5-1" == "$(printf "%s\\n12.5-1" "${TO_VERSION}" | sort -n | head -n1)" ] && [ "12.5-1" != "$(printf "%s\\n12.5-1" "${FROM_VERSION}" | sort -n | head -n1)" ]; then
+    printf "You are starting a major upgrade of the PostreSQL dogu (from %s to %s)!\\n" "${FROM_VERSION}" "${TO_VERSION}"
+    echo "Please consider a backup before doing so, e.g. via the Backup dogu!"
+    echo "During the upgrade process a full database dump will be created."
+    echo "Therefore please make sure your harddrive has at least as much free space left as your PostgreSQL database currently occupies!"
+fi

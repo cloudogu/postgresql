b84afc11645d18f0e67dc352ee13865e4a9790fd
meiserloh <matthias.eiserloh@cloudogu.com>
2025-07-24 15:50:21 +0200

    #52 - Clarified log output

--- resources/post-upgrade.sh
+++ resources/post-upgrade.sh
@@ -186,7 +186,7 @@ function killPostgresql() {
       # Postgres is still running
       sleep 0.1
     done
-    echo "postgresql successfully killed"
+    echo "postgresql successfully killed (this is expected during post upgrade)"
 }
 
 function runPostUpgrade() {
